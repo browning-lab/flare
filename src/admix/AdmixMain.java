c169ce792b308662cc6e98315b35f24db73e51d9
browning <browning@uw.edu>
2022-08-12 20:55:26 -0700

    Add version number.

--- src/admix/AdmixMain.java
+++ src/admix/AdmixMain.java
@@ -34,8 +34,8 @@ import java.util.Optional;
 public class AdmixMain {
 
     static final String EXECUTABLE = "flare.jar";
-    static final String REVISION = "flare.__REV__.jar";
-    static final String PROGRAM = EXECUTABLE + "  [ version 0.2.0, __REV__ ]";
+    static final String REVISION = "flare.12Aug22.faa.jar";
+    static final String PROGRAM = EXECUTABLE + "  [ version 0.2.0, 12Aug22.faa ]";
     static final String COPYRIGHT = "Copyright (C) 2022 Brian L. Browning";
     static final String COMMAND = "java -jar " + EXECUTABLE;
 
